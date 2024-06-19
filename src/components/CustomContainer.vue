fde3dfedf05e95ac9ef82222bf0d5ae032aa4e2b
BZ <BZ@example.com>
2024-06-20 00:39:01 +0800

    fix: auto title from type

--- src/components/CustomContainer.vue
+++ src/components/CustomContainer.vue
@@ -1,18 +1,31 @@
 <!-- fake custom component: https://github.com/vuejs/vitepress/blob/main/src/node/markdown/plugins/containers.ts -->
 <script setup>
+import { computed } from 'vue';
+
 const props = defineProps({
-  type: String,
-  title: String,
+  type: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    default: '',
+  },
 })
+
+const computedTitle = computed(() => {
+  return props.title || props.type[0].toUpperCase() + props.type.slice(1)
+})
+
 </script>
 
 <template>
   <details v-if="props.type === 'details'">
-    <summary :class="[props.type, 'custom-block']">{{ props.title }}</summary>
+    <summary :class="[props.type, 'custom-block']">{{ computedTitle }}</summary>
     <p><slot /></p>
   </details>
   <div :class="[props.type, 'custom-block']" v-else>
-    <p class="custom-block-title">{{ props.title }}</p>
+    <p class="custom-block-title">{{ computedTitle }}</p>
     <p><slot /></p>
   </div>
 </template>
