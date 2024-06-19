f0a74516051157f15a422356d7569433d689b525
BZ <BZ@example.com>
2024-06-20 01:34:56 +0800

    feat: CustomFeatures 元件 (源自 VPFeatures 元件)
    1. 所有 Props 皆與 Home Features 功能一致
    2. 預設是有左右 padding 的，設 noPadding: true，則會取消 padding
    
    範例：
    ```vue
    <CustomFeatures noPadding :features="[
      {
        icon: '📢',
        title: '現場公告',
        details: '今日活動已結束，請大家垃圾隨手帶走~<br>注意回家安全！',
        image: '/banner.png',
        link: '/about'
      },{
        icon: `ℹ️`,
        title: '活動資訊',
        details: `日期｜6/19 ~ 6/21<br>
        時間｜10:00 ~ 21:00<br>
        地點｜立法院青島東路及濟南路（靠中山南路）<br>
        訴求｜反國會濫權，拒絕民主倒退，警告中國國民黨<br>
        主辦｜台灣公民陣線、台灣經濟民主連合、公投護台灣聯盟<br>
        資訊來源：台灣經濟民主聯合<br>`,
        image: '/banner.png'
      }
    ]" />
    ```

---
+++ src/components/CustomFeatures.vue
@@ -0,0 +1,31 @@
+<script setup>
+import VPFeatures from 'vitepress/dist/client/theme-default/components/VPFeatures.vue';
+defineProps({
+  noPadding: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<template>
+  <VPFeatures class="cfeatures" :class="{ noPadding }" v-bind="$attrs" />
+</template>
+
+<style>
+/* style reset */
+.cfeatures.noPadding {
+  padding: inherit;
+}
+.cfeatures h2 {
+  margin: initial;
+  border-top: initial;
+  padding-top: initial;
+  letter-spacing: initial;
+  position: initial;
+  outline: initial;
+}
+.cfeatures p {
+  margin: inherit;
+}
+</style>
